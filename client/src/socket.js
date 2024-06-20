058094d1bd3493a68c8b651302d3779a5ef4a78a
Oliver Andersson <oliver.andersson101@gmail.com>
2024-06-20 04:35:59 +0200

    build

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.SOCKET_URL || "http://127.0.0.1:8081";
+const URL = "http://127.0.0.1:8081";
 
 export const socket = io(URL);
